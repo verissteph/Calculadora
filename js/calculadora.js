a5c247b3d8040ae6b9caffc20f12e11b92d22ad8
verissteph <stephanieverissimopp@gmail.com>
2020-04-04 15:31:18 -0300

    Nova função adicionada

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,11 +2,14 @@ let listaDeBotoes = document.querySelectorAll(".btn");
 let conteudoDoVisor = document.querySelector('#visor-interno');
 let opIgual = document.querySelector('.igual');
 let opApagar = document.querySelector('.apagar');
+let opBack = document.querySelector('.back');
 
 
 listaDeBotoes.forEach(botao => {
     botao.onclick = () => {
         if (botao.textContent == "=" || botao.textContent == "CE") {
+            return;
+        } else if (botao.textContent == " C ") {
             return;
         } else {
             conteudoDoVisor.innerHTML += botao.textContent
@@ -14,17 +17,25 @@ listaDeBotoes.forEach(botao => {
     }
 });
 
-opIgual.onclick =() =>{
-    if(conteudoDoVisor.textContent.indexOf('%') >= 0){
+opIgual.onclick = () => {
+    if (conteudoDoVisor.textContent.indexOf('%') >= 0) {
         // 10%50
-       let conteudo = conteudoDoVisor.textContent.split("%")
-      return conteudoDoVisor.innerHTML= conteudo[0]*conteudo[1] /100;
+        let conteudo = conteudoDoVisor.textContent.split("%")
+        return conteudoDoVisor.innerHTML = conteudo[0] * conteudo[1] / 100;
     }
-   let resultadoDaOp = eval(conteudoDoVisor.textContent)
-//    console.log(resultadoDaOp);
-conteudoDoVisor.innerHTML = resultadoDaOp;
+    let resultadoDaOp = eval(conteudoDoVisor.textContent)
+    //    console.log(resultadoDaOp);
+    conteudoDoVisor.innerHTML = resultadoDaOp;
 }
 
-opApagar.onclick = ()=>{
+opApagar.onclick = () => {
     conteudoDoVisor.innerHTML = " ";
+}
+
+opBack.onclick = () => {
+    let number = conteudoDoVisor.textContent;
+    number = number.substring(0, number.length - 1, 0);
+    conteudoDoVisor.innerHTML = number;
+
+
 }
